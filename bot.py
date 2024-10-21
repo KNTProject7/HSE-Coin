c5c352d21f25df111110f6a246f547c31ca19e9b
Phara <shtyrov311223@mail.ru>
2024-10-21 22:09:42 +0300

    add bot.py

---
+++ bot.py
@@ -0,0 +1,39 @@
+import asyncio
+
+from aiogram import Router, Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.types import Message, WebAppInfo
+from aiogram.filters import CommandStart
+from aiogram.enums import ParseMode
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+def webapp_builder() -> InlineKeyboardBuilder: 
+    builder = InlineKeyboardBuilder()
+    builder.button(
+        text="Let's Click!", web_app=WebAppInfo(
+            url="http://127.0.0.1:5500/index.html"
+        )
+    )
+
+    return builder.as_markup()
+
+router = Router()
+
+@router.message(CommandStart())
+async def start(message: Message) -> None:
+    await message.reply(
+        "Click! Click! Click!",
+        reply_markup=webapp_builder()
+    )
+
+async def main() -> None:
+    bot = Bot("7535373221:AAHcgr-bHLbZVpmrLUV_L6mKnb6DadqJGqw", default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+
+    dp = Dispatcher()
+    dp.include_router(router)
+
+    await bot.delete_webhook(True)
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
