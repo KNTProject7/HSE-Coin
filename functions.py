24e88dffda9d53a8b1d0b088decedde8051ee5ea
svliza <svalova_ea@mail.ru>
2024-10-24 21:36:43 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -21,3 +21,18 @@ with open('index.html') as file:
     template = Template(file.read())
 
 rendered_template = template.render(score = score)
+
+# with open('index.html', 'w', encoding='utf-8') as html_file:
+#     html_file.read()
+
+
+# tree = html.fromstring('index.html')  # сохранение HTML кода страницы в переменную
+# print(tree)
+# one = tree.xpath('/ins/text()')
+
+# driver = webdriver.Edge()
+# driver.get("https://translated.turbopages.org/proxy_u/en-ru.ru.a10e9b55-671a61ab-f170b68d-74722d776562/https/www.geeksforgeeks.org/selenium-webdriver-tutorial/")
+# first_form_input = driver.find_element_by_class_name("widget-area")
+# print(first_form_input)
+
+# user1 = 23455678
