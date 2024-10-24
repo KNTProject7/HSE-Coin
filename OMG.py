9064828f96b660cfd03a9f63667f33a9a4b7b60b
Ivoniy <Ivan2006033@gmail.com>
2024-10-24 19:21:43 +0300

    test

--- OMG.py
+++ OMG.py
@@ -2,12 +2,38 @@ import asyncio
 import sqlite3
 import telebot
 
+
+
 from aiogram import Router, Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.types import Message, WebAppInfo
 from aiogram.filters import CommandStart
 from aiogram.enums import ParseMode
 from aiogram.utils.keyboard import InlineKeyboardBuilder
+from selenium import webdriver
+from lxml import html # импорт библиотеки HTML парсера
+from webbrowser import *
+from jinja2 import Template
+
+score = 0
+
+with open('index.html') as file:
+    template = Template(file.read())
+
+rendered_template = template.render(score = score)
+
+# with open('index.html', 'w', encoding='utf-8') as html_file:
+#     html_file.read()
+
+
+# tree = html.fromstring('index.html')  # сохранение HTML кода страницы в переменную
+# print(tree)
+# one = tree.xpath('/ins/text()')
+
+# driver = webdriver.Edge()
+# driver.get("https://translated.turbopages.org/proxy_u/en-ru.ru.a10e9b55-671a61ab-f170b68d-74722d776562/https/www.geeksforgeeks.org/selenium-webdriver-tutorial/")
+# first_form_input = driver.find_element_by_class_name("widget-area")
+# print(first_form_input)
 
 # user1 = 23455678
 
@@ -39,10 +65,10 @@ cursor = conn.cursor()
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     if message.text.lower() == '/start':
-
         us_id = message.from_user.id
-
-        db_table_val(user_id=us_id)
+        info = cursor.execute('SELECT * FROM test WHERE user_id=?', (us_id, ))
+        if info.fetchone() is None:
+            db_table_val(user_id=us_id)
 
 bot.polling(none_stop=True)
 @router.message(CommandStart())
